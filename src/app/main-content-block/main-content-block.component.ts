241be07816e8888ca0215d2a223e1baef854b57a
juliamilano <t_yulya97@mail.ru>
2017-11-23 18:39:36 +0200

    add left block, input, array with tweets

--- src/app/main-content-block/main-content-block.component.ts
+++ src/app/main-content-block/main-content-block.component.ts
@@ -5,7 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './main-content-block.component.html',
   styleUrls: ['./main-content-block.component.css']
 })
+
+
 export class MainContentBlockComponent implements OnInit {
+  tweets: [{name: string, link: string, time: string, text: string}] = [{
+    name: "Ford",
+    link: "@mail",
+    time: "1h22m",
+    text: "Привет."
+  },
+    {
+    name: "Ford",
+    link: "@mail",
+    time: "1h52m",
+    text: "Привет.новпкркерерапраправр"
+  },
+    {
+    name: 'Ford',
+    link: "@mail",
+    time: "2h42m",
+    text: "liverpool dhngghhmdghmfffffffff роенове евоевоппо ен енеоенл лен."
+  }];
+
+  addTweets(tweet:{name: string, link: string, time: string, text: string}){
+    this.tweets.unshift(tweet);
+  }
 
   constructor() { }
 
